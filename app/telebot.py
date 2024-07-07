c4a44bc490eebc881c2b0df3dca2a2782cd17cfb
Mikhail Fomenko <mikhail.fomenko@media-tel.ru>
2024-07-07 22:15:46 +0300

    Added corrected stopping

--- app/telebot.py
+++ app/telebot.py
@@ -1,16 +1,30 @@
 import asyncio
-import os
-
-os.environ['CFG_PTH'] = '/work/git/github/helga_lash/telebot/dev-conf.yaml'
+import signal
 
 from tg import TelegramInterface
 from scheduler import schedule
+from configuration import logger, apl_conf
+
+
+async def shutdown(incoming_signal: signal.Signals, tg: TelegramInterface):
+    """Cleanup tasks tied to the service's shutdown."""
+    logger.info(f"Received exit signal {incoming_signal.name}...")
+    apl_conf.run = False
+    await tg.dispatcher.stop_polling()
+    logger.info("Closing database connections")
 
 
 async def main():
+    apl_conf.run = True
     tg: TelegramInterface = TelegramInterface()
-    bot_task = tg.run()
+    bot_task = asyncio.create_task(tg.run())
     scheduler_task = asyncio.create_task(schedule(tg.bot))
-    await asyncio.gather(bot_task, scheduler_task)
+    task_list = [bot_task, scheduler_task]
+    loop = asyncio.get_running_loop()
+    for incoming_signal in (signal.SIGHUP, signal.SIGTERM, signal.SIGINT):
+        loop.add_signal_handler(
+            incoming_signal, lambda: asyncio.create_task(shutdown(incoming_signal, tg)))
+    await asyncio.gather(*task_list)
+
 
 asyncio.run(main())
